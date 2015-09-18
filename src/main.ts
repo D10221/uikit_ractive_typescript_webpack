1d55e1985ad54ddb1e39d94a4f861a6b3bdce073
Daniel <danieliozzi@gmail.com>
2015-09-18 13:53:14 +0200

    Added Ractive, webpack, typescript , built/main.js

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,43 @@
-// ???
+import Ractive = require('ractive');
+
+var AppFooter = Ractive.extend({
+  
+  isolated: false,
+  template: require('../templates/appFooter.html')
+
+});
+
+var OffCanvasBar = Ractive.extend({
+  isolated:false,
+  template: require('../templates/offCanvasBar.html')
+});
+
+var TopNavBar = Ractive.extend({
+  isolated:false,
+  template: require('../templates/topNavBar.html')
+});
+
+var WelCome = Ractive.extend({
+  isolated:false,
+  template:require('../templates/welcome.html')
+});
+/** 
+ * APP
+ */
+var ractive = new Ractive({
+  
+  el: '#container',
+  template: require('../templates/appContainer.html'),
+  data: { 
+    greeting: 'Hello',
+    recipient: 'world' 
+    },
+    
+  components:{
+    AppFooter: AppFooter,
+    OffCanvasBar: OffCanvasBar,
+    TopNavBar:TopNavBar,
+    WelCome: WelCome
+    
+    }
+});
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,1 +1,3 @@
 
+/// <reference path="ractive/ractive.d.ts" />
+/// <reference path="requireme.d.ts" />
